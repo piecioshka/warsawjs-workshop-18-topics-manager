73c9ed07cdd54394a9847f42a2e7081ef73f2a02
Piotr Kowalski <piecioshka@gmail.com>
2023-01-20 18:27:38 +0100

    Remove TODO

--- scripts/repositories/storage-repository.js
+++ scripts/repositories/storage-repository.js
@@ -1,7 +1,6 @@
 const LocalStorageRepository = require('./local-storage-repository');
 const HttpStorageRepository = require('./http-storage-repository');
 
-module.exports = LocalStorageRepository;
-
-// TODO(piecioshka): do zrobienia
-// module.exports = HttpStorageRepository;
+module.exports = HttpStorageRepository;
+// You can use it:
+// module.exports = LocalStorageRepository;
